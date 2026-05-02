2f38d3f0f5996b881c7e6c0db0606bed4f222c2b
Alberto Fernández <152698267+lumeou@users.noreply.github.com>
2026-05-02 20:22:10 +0200

    Update binary_sensor.py

--- custom_components/energy_periods/binary_sensor.py
+++ custom_components/energy_periods/binary_sensor.py
@@ -6,23 +6,67 @@ async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
     async_add_entities([
-        HolidayBinarySensor(coordinator, entry.entry_id)
+        WeekendBinarySensor(coordinator, entry.entry_id),
+        HolidayBinarySensor(coordinator, entry.entry_id),
+        NonWorkingDayBinarySensor(coordinator, entry.entry_id),
     ])
 
 
-class HolidayBinarySensor(BinarySensorEntity):
+class WeekendBinarySensor(BinarySensorEntity):
+
     def __init__(self, coordinator, entry_id):
         self.coordinator = coordinator
-        self._attr_unique_id = f"{entry_id}_holiday_today"
+        self._attr_unique_id = f"{entry_id}_weekend"
 
     @property
     def name(self):
-        return "Is holiday today"
+        return "Is weekend"
 
     @property
     def icon(self):
-        return "mdi:calendar"
-    
+        return "mdi:calendar-weekend"
+
     @property
     def is_on(self):
-        return self.coordinator.is_today_holiday()
+        return self.coordinator.is_weekend()
+
+
+class HolidayBinarySensor(BinarySensorEntity):
+
+    def __init__(self, coordinator, entry_id):
+        self.coordinator = coordinator
+        self._attr_unique_id = f"{entry_id}_holiday"
+
+    @property
+    def name(self):
+        return "Is public holiday"
+
+    @property
+    def icon(self):
+        return "mdi:calendar-star"
+
+    @property
+    def is_on(self):
+        return self.coordinator.is_public_holiday()
+
+
+class NonWorkingDayBinarySensor(BinarySensorEntity):
+
+    def __init__(self, coordinator, entry_id):
+        self.coordinator = coordinator
+        self._attr_unique_id = f"{entry_id}_non_working_day"
+
+    @property
+    def name(self):
+        return "Is non-working day"
+
+    @property
+    def icon(self):
+        return "mdi:calendar-remove"
+
+    @property
+    def is_on(self):
+        return (
+            self.coordinator.is_weekend()
+            or self.coordinator.is_public_holiday()
+        )
