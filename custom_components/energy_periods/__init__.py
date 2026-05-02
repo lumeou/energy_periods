a0b5719486d60319169453a0b20cf6e50720cabe
Alberto Fernández <152698267+lumeou@users.noreply.github.com>
2026-05-02 22:32:38 +0200

    Update __init__.py

--- custom_components/energy_periods/__init__.py
+++ custom_components/energy_periods/__init__.py
@@ -10,7 +10,7 @@ async def async_setup_entry(hass, entry):
     sources = entry.data.get("sources", [])
     config = entry.options.get("periods", DEFAULT_CONFIG)
 
-    _LOGGER.debug(config)
+    _LOGGER.debug("Config: %s", config)
 
     providers = []
 
