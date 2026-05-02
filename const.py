3e5428e66f088a444802f8b8f417097317547a15
Alberto Fernández <152698267+lumeou@users.noreply.github.com>
2026-05-02 09:16:39 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,11 @@
+DOMAIN = "energy_periods"
+
+DEFAULT_CONFIG = {
+    "workday": [
+        {"start": "10:00", "end": "14:00", "period": "punta"},
+        {"start": "18:00", "end": "22:00", "period": "punta"}
+    ],
+    "holiday": [
+        {"start": "00:00", "end": "24:00", "period": "valle"}
+    ]
+}
