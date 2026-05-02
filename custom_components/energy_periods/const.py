2b85e19238c9263f684c8019b9b54f6e0b640722
Alberto Fernández <152698267+lumeou@users.noreply.github.com>
2026-05-02 22:02:29 +0200

    Update const.py

--- custom_components/energy_periods/const.py
+++ custom_components/energy_periods/const.py
@@ -1,11 +1,11 @@
 DOMAIN = "energy_periods"
 
 DEFAULT_CONFIG = {
-    "workday": [
+    "working_day": [
         {"start": "10:00", "end": "14:00", "period": "punta"},
         {"start": "18:00", "end": "22:00", "period": "punta"}
     ],
-    "holiday": [
+    "non_working_day": [
         {"start": "00:00", "end": "24:00", "period": "valle"}
     ]
 }
